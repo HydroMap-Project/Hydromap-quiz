8f916eb767369a5be113a1fdc68018a5572e086a
Guilherme Camacho <guilherme.camachobr@gmail.com>
2026-06-07 18:10:19 -0300

    feat: adicionar logica de validacao do formulario

--- script.js
+++ script.js
@@ -16,3 +16,49 @@ if(slide) {
 slide.src = imagens[indiceImagem];
 }
 },3000);
+
+const temas = {
+escuro:{
+bg:"#121212",
+card:"#1f1f1f",
+text:"#ffffff",
+primary:"#2563eb"
+},
+azul:{
+bg:"#0b132b",
+card:"#1c2541",
+text:"#ffffff",
+primary:"#3a86ff"
+},
+verde:{
+bg:"#022c22",
+card:"#064e3b",
+text:"#ffffff",
+primary:"#10b981"
+}
+};
+
+document.querySelectorAll(".tema").forEach(btn=>{
+btn.addEventListener("click",()=>{
+const tema = temas[btn.dataset.tema];
+document.documentElement.style.setProperty("--bg",tema.bg);
+document.documentElement.style.setProperty("--card",tema.card);
+document.documentElement.style.setProperty("--text",tema.text);
+document.documentElement.style.setProperty("--primary",tema.primary);
+});
+});
+
+document.getElementById("contatoForm").addEventListener("submit",(e)=>{
+e.preventDefault();
+const nome = document.getElementById("nome").value.trim();
+const email = document.getElementById("email").value.trim();
+if(nome === ""){
+alert("Digite o nome");
+return;
+}
+if(!email.includes("@")){
+alert("Digite um email válido");
+return;
+}
+alert("Formulário enviado com sucesso");
+});
